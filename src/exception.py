761beb941ddb77ad183f1c46679387b812bf9254
iamgopinathbehera <beheragopinath376@gmail.com>
2024-09-18 20:48:59 +0530

    EDA and Problem statement

--- src/exception.py
+++ src/exception.py
@@ -21,3 +21,14 @@ class CustomException(Exception):
     
 
 
+if __name__ == "__main__":
+
+  try: 
+    a = 1/0
+
+  except Exception as e:
+    logging.info("Divide by Zero")
+    raise CustomException(e,sys) 
+
+   
+
